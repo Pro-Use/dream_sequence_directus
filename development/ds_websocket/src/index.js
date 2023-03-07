5322690b611d59bb534b402ff32f5a9da1867b56
Pro-Use <robertprouse@gmail.com>
2023-03-07 14:43:15 +0000

    websocket uses namespace + logs incomming emits

--- development/ds_websocket/src/index.js
+++ development/ds_websocket/src/index.js
@@ -31,13 +31,19 @@ export default async ({ schedule, action }, {database, getSchema}) => {
 		  socket.emit("hello", "browser", (response) => {
 		    console.log(response);
 		  });
+
+		  // receive data
+		  socket.on("data", (data) => {
+		  	console.log(data)
+		  });
 		});
 	});
 
 	schedule('*/10 * * * * *', async () => {
-		let outputs = await database('outputs').select('*').where('clients', 'unreal');
-		// console.log(outputs)
-		await outputs.forEach( async (output) => {
+		// UE
+		let ue_outputs = await database('outputs').select('*').where('clients', 'unreal');
+		// console.log(ue_outputs)
+		await ue_outputs.forEach( async (output) => {
 			// console.log(output)
 			let min = await database(output.data_source).min(output.data_type + ' as min');
 			let max = await database(output.data_source).max(output.data_type + ' as max');
@@ -49,6 +55,22 @@ export default async ({ schedule, action }, {database, getSchema}) => {
 			}
 			console.log(output.name+': '+data_point)
 			io.emit(output.name, data_point)
+			io.of('/unreal').emit(output.name, data_point)
+		})
+		// Midi
+		let midi_outputs = await database('outputs').select('*').where('clients', 'midi');
+		// console.log(midi_outputs)
+		await midi_outputs.forEach( async (output) => {
+			// console.log(output)
+			let min = await database(output.data_source).min(output.data_type + ' as min');
+			let max = await database(output.data_source).max(output.data_type + ' as max');
+			if (min[0].min == max[0].max){
+				var data_point = 0.5;
+			} else {
+				let data_real = await database(output.data_source).orderBy('id', 'desc').first(output.data_type);
+				var data_point = scale(data_real[output.data_type], min[0].min, max[0].max, output.min, output.max)
+			}
+			io.of('/midi').emit(output.name, data_point)
 		})
 	});
 };
