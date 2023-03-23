e7ba853215b52438d315c5097f17cd3f56003413
Pro-Use <robertprouse@gmail.com>
2023-03-23 17:24:01 +0000

    first data limiter build

---
+++ extensions/hooks/ds_limit_data/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var index = ({ action }, {database}) => {
+
+	const max_data = 7200;
+
+	action('items.create', async ({collection, key}) => {
+		console.log('item created for '+ collection);
+		if (collection == 'urad' || collection == 'water_monitor'){
+			let data_count = await(database(collection).count('*', {as: 'count'}));
+			console.log(data_count);
+			if (data_count.length ){
+				let first_id = key - max_data;
+				if (data_count[0].count > max_data) {
+					let del_rows = await(database('urad').where('id', '<=', first_id).del());
+					console.log('deleting: '+ del_rows);
+				}
+			}
+		}
+	});
+};
+
+module.exports = index;
